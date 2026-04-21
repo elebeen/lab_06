f1a685f2f44b861523b05eda0e682e69019d58e5
hermes curo <hermescurov5@agmail.com>
2026-04-20 19:07:35 -0500

    Adding auth controller logic

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,4 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using lab_06.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace lab_06.Controllers;
 
@@ -12,4 +18,43 @@ public class AuthController : ControllerBase
     {
         _configuration = configuration;
     }
+
+    [HttpPost("login")]
+    public IActionResult Login([FromBody] User login)
+    {
+        if (login.Name.ToString() == "admin" && login.Password.ToString() == "admin")
+        {
+            var claims = new[]
+            {
+                new Claim(type: ClaimTypes.Name, login.Name.ToString()),
+                new Claim(type: ClaimTypes.Role, "Admin")
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes
+                (_configuration["Jwt:SecretKey"]));
+            var creds = new SigningCredentials(key, algorithm: SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds);
+
+            return Ok(new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token)
+            });
+        }
+
+        return Unauthorized();
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpGet("admin")]
+    public IActionResult GetAdmin()
+    {
+        return Ok("solo admins");
+    }
+    
 }
---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab_06.Models;
+
+public partial class User
+{
+    public long Id { get; set; }
+
+    public char Name { get; set; }
+
+    public char Password { get; set; }
+}
